bd3a0bb92993d14217ba24417433cdcb87dac122
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-08-19 09:52:44 -0500

    [UPDATE]challenge done

--- src/index.js
+++ src/index.js
@@ -6,11 +6,29 @@
  * @param {number} startFrom - index of the candidate to start further exploration from.
  * @return {number[][]}
  */
-
+let result = [];
 const combinationSumRecursive = (
-    
+  candidates,
+  target,
+  results = [],) => { let solution = results.reduce((a, b) => a + b, 0);
+
+  if (solution === target) {
+    result.push(Array.from(results))
   }
-  
+  if (solution >= target) {
+    return;
+  }
+
+  for (let i = 0; i < candidates.length; i++) {
+    let num = candidates[i];
+    let candidatesTests = candidates.slice(i);
+    combinationSumRecursive(candidatesTests, target, results.concat([num]));
+
+  }
+  return result;
+};
+
+
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
    *
@@ -18,8 +36,9 @@ const combinationSumRecursive = (
    * @param {number} target
    * @return {number[][]}
    */
-const combinationSum = (candidates, target) => {
+  const combinationSum = (candidates, target) => {
+    result = [];
     return combinationSumRecursive(candidates, target);
-  }
+  };
 
 module.exports = combinationSum;
